a3a7d025783c6da81267ca1dbfc8fb7b84086b3d
Rick Taylor <rick@tropicalstormsoftware.com>
2012-01-16 12:05:26 +0000

    Updates to makefile and OOBase
    Signed-off-by: Rick Taylor <rick@tropicalstormsoftware.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,5 @@
 #! /bin/sh
 echo Running autotools...
-
-if test ! -d "./ookv"; then
-	echo You need to link ookv to ./ookv, try ln -s
-fi
-
-if test ! -d "./oobase"; then
-	echo You need to link oobase to ./oobase, try ln -s
-fi
-
 aclocal -I m4 && \
 autoheader && \
 libtoolize --force --no-warn && \
